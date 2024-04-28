94b61b8994b522f25c02c6eb2f08b54875c08958
saelon <saelon@utu.fi>
2024-04-28 11:21:33 +0300

    like sort()

--- src/components/BlogList.jsx
+++ src/components/BlogList.jsx
@@ -2,10 +2,16 @@ import React from 'react'
 import Blog from './Blog'
 
 const BlogList = ({ blogs, addLikes }) => {
+
+  const compareLikes = (blogA, blogB) => {
+    return blogB.likes - blogA.likes
+  }
+
+  const sortedBlogs = blogs.slice().sort(compareLikes)
   return (
     <div>
       <h2>Blogs</h2>
-      {blogs.map(blog =>
+      {sortedBlogs.map(blog =>
         <Blog key={blog.id} blog={blog} addLikes={addLikes} />
       )}
     </div>
